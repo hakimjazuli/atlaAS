1077de057c8cff3e2fadade7c4e0d3961a6f0e6c
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-04-30 13:58:44 +0700

    app

--- src/router/Routes_.php
+++ src/router/Routes_.php
@@ -5,6 +5,6 @@ namespace HtmlFirst\atlaAS\Router;
 use HtmlFirst\atlaAS\App;
 
 abstract class Routes_ {
-    public function __construct(private App $app) {
+    public function __construct(protected App $app) {
     }
 }
