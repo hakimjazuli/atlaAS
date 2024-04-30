fda25e39461be7d26bf8552a64eebbe705fb024e
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-04-30 15:15:44 +0700

    need changes

--- src/vars/AppEnv.php
+++ src/vars/AppEnv.php
@@ -3,4 +3,5 @@
 namespace HtmlFirst\atlaAS\Vars;
 
 abstract class AppEnv {
+    public static $is_in_production = false;
 }
--- src/vars/AppSettings.php
+++ src/vars/AppSettings.php
@@ -2,9 +2,31 @@
 
 namespace HtmlFirst\atlaAS\Vars;
 
+use HtmlFirst\atlaAS\App;
+
 abstract class AppSettings {
+    public function __construct(private App $app) {
+    }
     public string $middleware_name = 'mw';
     public string $routes_path = 'routes';
     public string $routes_class = '\\Routes';
-    public bool $no_indent_html = false;
+
+    public static $chunk_sizes = 204_800;
+    public static $refresh_micro_second = 500_000;
+    public static $load_file_with_php_require = false;
+    public static $system_file = 'php';
+    public static $use_stream = true;
+
+    public static function server_ip() {
+        return @$_SERVER['SERVER_ADDR'];
+    }
+    public static function system_path(string $path): string {
+        return str_replace(['/', '\\'], [\DIRECTORY_SEPARATOR, \DIRECTORY_SEPARATOR], $path);
+    }
+    public function use_caching() {
+        return [$this->if_in_production(true, true), 60/* days */];
+    }
+    public function if_in_production(bool $in_production_value, bool $not_in_production_value): bool {
+        return $this->app->app_env::$is_in_production ? $in_production_value : $not_in_production_value;
+    }
 }
