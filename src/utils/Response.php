df67c9440be014b987698d7d791fef7b0547528e
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-04-30 14:41:40 +0700

    check

--- src/utils/Response.php
+++ src/utils/Response.php
@@ -4,18 +4,20 @@ namespace HtmlFirst\atlaAS\Utils;
 
 class Response {
     use hasAppRegex;
-    private static function echo_preprocess(callable $html_function) {
-        \header("Content-Type: text/html; charset=UTF-8");
+    private static function echo_preprocess(callable $html_function, bool $html_document = true) {
+        if ($html_document) {
+            \header("Content-Type: text/html; charset=UTF-8");
+        }
         \ob_start();
         $html_function();
         return \ob_get_clean();
     }
-    public static function echo_no_indent(callable $html_function) {
-        $output = self::echo_preprocess($html_function);
+    public static function echo_no_indent(callable $html_function, bool $html_document = true) {
+        $output = self::echo_preprocess($html_function, $html_document);
         echo \preg_replace(self::$no_indents, '', $output);
     }
-    public static function echo_single_line(callable $html_function) {
-        $output = self::echo_preprocess($html_function);
+    public static function echo_single_line(callable $html_function, bool $html_document = true) {
+        $output = self::echo_preprocess($html_function, $html_document);
         echo \preg_replace(self::$single_line, '', $output);
     }
     public static function print_json(array $array, bool $as_json_api = true): void {
--- src/utils/hasAppRegex.php
+++ src/utils/hasAppRegex.php
@@ -3,29 +3,29 @@
 namespace HtmlFirst\atlaAS\Utils;
 
 trait hasAppRegex {
-    public static function regex_php_to_html(string $php_regex) {
+    protected static function regex_php_to_html(string $php_regex) {
         return trim($php_regex, "/");
     }
-    public static $single_line = '/\/\*[\s\S]*?\*\/|<!--[\s\S]*?-->|\r|\n|\t|\s{2,}|\/\/ .*$/m';
-    public static $excesive_spacing = '/\s{2,}/m';
-    public static $html_tag_spacing = '/\s*(?=[><\/])/mU';
-    public static $excesive_html_br = '/(<br>){2,}/';
-    public static $no_indents = '/^[\s]+| {2,}/m';
-    public static function tag(string $tag_name) {
+    protected static $single_line = '/\/\*[\s\S]*?\*\/|<!--[\s\S]*?-->|\r|\n|\t|\s{2,}|\/\/ .*$/m';
+    protected static $excesive_spacing = '/\s{2,}/m';
+    protected static $html_tag_spacing = '/\s*(?=[><\/])/mU';
+    protected static $excesive_html_br = '/(<br>){2,}/';
+    protected static $no_indents = '/^[\s]+| {2,}/m';
+    protected static function tag(string $tag_name) {
         return "/<$tag_name(.*?)>|<\/$tag_name>/m";
     }
-    public static function alphanumeric_loose(int $min, int $max) {
+    protected static function alphanumeric_loose(int $min, int $max) {
         return '/^(?=.*[a-zA-Z0-9]).{' . $min . ',' . $max . '}$/';
     }
-    public static function alpha_loose(int $min, int $max) {
+    protected static function alpha_loose(int $min, int $max) {
         return '/^(?=.*[a-zA-Z]).{' . $min . ',' . $max . '}$/';
     }
-    public static function numeric_loose(int $min, int $max) {
+    protected static function numeric_loose(int $min, int $max) {
         return '/^[0-9]{' . $min . ',' . $max . '}$/';
     }
-    public static $float = '/^[-|\.|0-9]{1,}$/';
-    public static $non_alphanumeric = '/[^a-zA-Z0-9_]+/';
-    public static function enum(...$allowed_values): string {
+    protected static $float = '/^[-|\.|0-9]{1,}$/';
+    protected static $non_alphanumeric = '/[^a-zA-Z0-9_]+/';
+    protected static function enum(...$allowed_values): string {
         $escaped_values = array_map('preg_quote', $allowed_values);
         $pattern = '/^(' . implode('|', $escaped_values) . ')$/';
         return $pattern;
