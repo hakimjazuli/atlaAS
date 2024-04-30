34aa32f650cf6d131f6dff638f35f0efb45b4d53
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-04-30 11:13:54 +0700

    main

--- src/App.php
+++ src/App.php
@@ -12,10 +12,12 @@ use HtmlFirst\atlaAS\Vars\AppEnv;
 class App {
     public Request $request;
     public string $app_root;
+    public string $public_url_root;
     public Response $response;
     public function __construct(public AppEnv $app_env, public AppSettings $app_settings) {
         $this->request = new Request;
         $this->app_root = \dirname($this->request->public_path);
+        $this->public_url_root = $this->request->http_mode . '://' . $_SERVER['HTTP_HOST'] . '/';
         $this->response = new Response;
     }
     public function run(CLI|false $cli = false) {
