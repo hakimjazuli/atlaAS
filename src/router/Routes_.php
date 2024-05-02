41e484f91041dc4aeeed7aba3f63b37c0029a68a
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-05-02 23:29:30 +0700

    no query param just use props

--- src/router/Routes_.php
+++ src/router/Routes_.php
@@ -6,8 +6,4 @@ use HtmlFirst\atlaAS\Utils\hasProtectedApp;
 
 abstract class Routes_ {
     use hasProtectedApp;
-    protected function with_query_param(array $query_param) {
-        $this->app->request->overwite_param = $query_param;
-        return $this;
-    }
 }
