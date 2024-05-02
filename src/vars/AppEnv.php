b1d6c64f230ba9c7ae4815e4fde44057c85c2f14
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-05-02 16:10:43 +0700

    env

--- src/vars/AppEnv.php
+++ src/vars/AppEnv.php
@@ -4,4 +4,10 @@ namespace HtmlFirst\atlaAS\Vars;
 
 abstract class AppEnv {
     public static $is_in_production = false;
+
+    public static $db_driver = 'mysql';
+    public static $db_host = 'localhost';
+    public static $db_database = 'palusidas';
+    public static $db_username = 'root';
+    public static $db_password = '';
 }
