d959640bf5577727a70ee443bf90a59f1a634adc
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-05-02 22:34:28 +0700

    env base

--- src/vars/AppEnv.php
+++ src/vars/AppEnv.php
@@ -6,32 +6,4 @@ abstract class AppEnv {
     public static $is_in_production = false;
 
     public static $app_key = '4arrW44EN4T5YIYTytcyrbuAERbrIb5e48Y7Ro8y9WYEY8b69';
-
-    public static string $default_connection = 'site';
-    public static array $conn = [
-        'http' => [
-            'site' =>
-            [
-                'host' => 'localhost',
-                'username' => 'root',
-                'password' => '',
-                'db' => '',
-                'type' => 'mysql',
-                'file_name' => '',
-                'encoding' => '',
-            ]
-        ],
-        'https' => [
-            'site' =>
-            [
-                'host' => '',
-                'username' => '',
-                'password' => '',
-                'db' => '',
-                'type' => '',
-                'file_name' => '',
-                'encoding' => '',
-            ]
-        ]
-    ];
 }
