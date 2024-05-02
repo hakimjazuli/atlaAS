85dbd41a6b8b293834a3ff6611170080e290150b
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-05-02 23:12:13 +0700

    main

--- src/router/Routes_.php
+++ src/router/Routes_.php
@@ -6,19 +6,4 @@ use HtmlFirst\atlaAS\Utils\hasProtectedApp;
 
 abstract class Routes_ {
     use hasProtectedApp;
-    public function __call($method, $args) {
-        if ($method === $this->app->request->method) {
-            $this->self_mw_check();
-        }
-    }
-    private function self_mw_check() {
-        if (\method_exists(
-            $this,
-            $mw = $this->app->app_settings->middleware_name
-        )) {
-            $mw($this->app->request->method);
-        }
-    }
-    private function recursive_mw_call() {
-    }
 }
