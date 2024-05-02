efb6711d8b89ab98cc27c62150f412962827d976
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-05-03 00:00:36 +0700

    main

--- src/router/Routes_.php
+++ src/router/Routes_.php
@@ -6,6 +6,9 @@ use HtmlFirst\atlaAS\Utils\hasProtectedApp;
 
 abstract class Routes_ {
     use hasProtectedApp;
+    /**
+     * use this only for calling it's own get method;
+     */
     protected function with_query_param(array $query_param) {
         $this->app->request->overwite_param = $query_param;
         return $this;
