165087fd2d44590a5eae725fcab3012a5cabb258
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-05-04 16:00:14 +0700

    main

--- src/connection/FieldType_.php
+++ src/connection/FieldType_.php
@@ -2,7 +2,7 @@
 
 namespace HtmlFirst\atlaAS\Connection;
 
-class FieldType {
+class FieldType_ {
     public function __construct(public int $type, public string|null $regex = null, public string|null $regex_html = null) {
     }
 }
--- src/connection/Table_.php
+++ src/connection/Table_.php
@@ -5,14 +5,14 @@ namespace HtmlFirst\atlaAS\Connection;
 use HtmlFirst\atlaAS\Utils\hasAppRegex;
 
 /**
- * assign all parameter type as \HtmlFirst\atlaAS\Connection\FieldType;
- * eg. public FieldType user_name;
+ * assign all parameter type as \HtmlFirst\atlaAS\Connection\FieldType_;
+ * eg. public FieldType_ $field_name_alias;
  * then on constructor assign it by calling $this->column(...$neccessary_args);
  */
 abstract class Table_ {
     use hasAppRegex;
-    protected function column(int $pdo_param_type, string|null $regex = null): FieldType {
-        return new FieldType(
+    protected function column(int $pdo_param_type, string|null $regex = null): FieldType_ {
+        return new FieldType_(
             $pdo_param_type,
             $regex,
             $regex !== null ? $this->regex_php_to_html($regex) : null
