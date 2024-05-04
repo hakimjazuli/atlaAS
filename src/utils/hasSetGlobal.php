1dc000f32059184496dbf41c4695194b3f5cb386
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-05-05 02:46:41 +0700

    static

--- src/utils/hasSetGlobal.php
+++ src/utils/hasSetGlobal.php
@@ -2,11 +2,17 @@
 
 namespace HtmlFirst\atlaAS\Utils;
 
+use ReflectionClass;
+
 trait hasSetGlobal {
     public function set_as_global() {
         $instance = 'instance';
         if (\property_exists($this, $instance)) {
-            static::$$instance = $this;
+            $reflection_class = new ReflectionClass($this::class);
+            $static_roperties = $reflection_class->getStaticProperties();
+            if (array_key_exists('staticProperty', $static_roperties)) {
+                static::$$instance = $this;
+            }
         }
     }
 }
