636df2a77789dcaa3be5574eede163b0909e9a62
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-05-04 23:16:08 +0700

    global

--- src/utils/hasGlobalChecker_.php
+++ src/utils/hasGlobalChecker_.php
@@ -3,16 +3,16 @@
 namespace HtmlFirst\atlaAS\Utils;
 
 trait hasGlobalChecker_ {
-    protected string $name;
+    protected string $global;
 
     use hasProtectedApp_;
     protected function global(string $name, mixed $initial_value) {
         if (!\is_string($this->name)) {
             return null;
         }
-        if (isset($this->app->global[$this->identifier][$name])) {
-            return $this->app->global[$this->identifier][$name];
+        if (isset($this->app->global[$this->name][$name])) {
+            return $this->app->global[$this->name][$name];
         }
-        return ($this->app->global[$this->identifier][$name] = $initial_value);
+        return ($this->app->global[$this->name][$name] = $initial_value);
     }
 }
