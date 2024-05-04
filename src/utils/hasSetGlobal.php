b7ce7c2f3efffa37b9aa11c11c7d53a6520cb4cc
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-05-05 01:14:12 +0700

    fixed

--- src/utils/hasSetGlobal.php
+++ src/utils/hasSetGlobal.php
@@ -5,7 +5,7 @@ namespace HtmlFirst\atlaAS\Utils;
 trait hasSetGlobal {
     public function set_as_global() {
         $instance = 'instance';
-        if (\method_exists($this, $instance)) {
+        if (\property_exists($this, $instance)) {
             static::$$instance = $this;
         }
     }
