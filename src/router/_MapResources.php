145bacf84595a6cb8c76f38737a9d43dddb9bc44
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-05-05 18:46:43 +0700

    anotate for use and middleware

--- src/router/_MapResources.php
+++ src/router/_MapResources.php
@@ -2,7 +2,15 @@
 
 namespace HtmlFirst\atlaAS\Router;
 
+/**
+ * special routes to map the resoruces of the same name folder:
+ * - route file: $routename.php;
+ * - resource path: "./$routename/*";
+ */
 abstract class _MapResources {
+    /**
+     * overwrite this get method to use it as this route middleware;
+     */
     public function get(string ...$uri_array) {
     }
 }
