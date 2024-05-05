3f4c35b11b0180436dd718ebc693ad14c9f7328a
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-05-05 13:21:35 +0700

    static

--- src/utils/_GlobalVar.php
+++ src/utils/_GlobalVar.php
@@ -5,15 +5,15 @@ namespace HtmlFirst\atlaAS\Utils;
 use HtmlFirst\atlaAS\__atlaAS;
 
 abstract class _GlobalVar {
-    protected string $global = 'global';
+    protected static string $global = 'global';
 
-    protected function global(string $name, mixed $initial_value) {
-        if (!\is_string($this->global)) {
+    protected static function global(string $name, mixed $initial_value) {
+        if (!\is_string(self::$global)) {
             return null;
         }
-        if (isset(__atlaAS::$__->global[$this->global][$name])) {
-            return __atlaAS::$__->global[$this->global][$name];
+        if (isset(__atlaAS::$__->global[self::$global][$name])) {
+            return __atlaAS::$__->global[self::$global][$name];
         }
-        return (__atlaAS::$__->global[$this->global][$name] = $initial_value);
+        return (__atlaAS::$__->global[self::$global][$name] = $initial_value);
     }
 }
