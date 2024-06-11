0eb92f53d5490d80c3bef013f24cfe65f53301d2
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-06-11 22:25:12 +0700

    cors call method

--- src/utils/_Cors.php
+++ src/utils/_Cors.php
@@ -18,43 +18,40 @@ class _Cors {
      * - in days
      * @return void
      */
-    public function __construct(
-        private array $allowed_origins = [],
-        private array $allowed_methods = [],
-        private array  $allowed_headers = [],
-        private float $max_age = 1,
+    public static function allow(
+        array $allowed_origins = [],
+        array $allowed_methods = [],
+        array $allowed_headers = [],
+        float $max_age = 1,
     ) {
-        $this->handle();
-    }
-    private function handle() {
         $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
-        if (in_array('*', $this->allowed_origins)) {
+        if (in_array('*', $allowed_origins)) {
             header("Access-Control-Allow-Origin: *");
-        } elseif (in_array($origin, $this->allowed_origins)) {
+        } elseif (in_array($origin, $allowed_origins)) {
             header("Access-Control-Allow-Origin: " . $origin);
         }
-        if (in_array('*', $this->allowed_methods)) {
+        if (in_array('*', $allowed_methods)) {
             header("Access-Control-Allow-Methods: *");
-        } elseif (!empty($this->allowed_methods)) {
-            header("Access-Control-Allow-Methods: " . implode(", ", $this->allowed_methods));
+        } elseif (!empty($allowed_methods)) {
+            header("Access-Control-Allow-Methods: " . implode(", ", $allowed_methods));
         }
-        if (in_array('*', $this->allowed_headers)) {
+        if (in_array('*', $allowed_headers)) {
             header("Access-Control-Allow-Headers: *");
-        } elseif (!empty($this->allowed_headers)) {
-            header("Access-Control-Allow-Headers: " . implode(", ", $this->allowed_headers));
+        } elseif (!empty($allowed_headers)) {
+            header("Access-Control-Allow-Headers: " . implode(", ", $allowed_headers));
         }
         if (__Request::$method == 'options') {
-            if (in_array('*', $this->allowed_methods)) {
+            if (in_array('*', $allowed_methods)) {
                 header("Access-Control-Allow-Methods: *");
-            } elseif (!empty($this->allowed_methods)) {
-                header("Access-Control-Allow-Methods: " . implode(", ", $this->allowed_methods));
+            } elseif (!empty($allowed_methods)) {
+                header("Access-Control-Allow-Methods: " . implode(", ", $allowed_methods));
             }
-            if (in_array('*', $this->allowed_headers)) {
+            if (in_array('*', $allowed_headers)) {
                 header("Access-Control-Allow-Headers: *");
-            } elseif (!empty($this->allowed_headers)) {
-                header("Access-Control-Allow-Headers: " . implode(", ", $this->allowed_headers));
+            } elseif (!empty($allowed_headers)) {
+                header("Access-Control-Allow-Headers: " . implode(", ", $allowed_headers));
             }
-            header('Access-Control-Max-Age: ' . $this->max_age * 86400);
+            header('Access-Control-Max-Age: ' . $max_age * 86400);
             exit(0);
         }
     }
